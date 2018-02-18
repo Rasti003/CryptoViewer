00523c3e9325ec983edc93896b0719b761a9f727
Rasti003 <przemyslaw.murzyn@outlook.com>
2018-02-18 14:30:06 +0100

    -added currency rounding

--- CryptoViewerMain.py
+++ CryptoViewerMain.py
@@ -1,24 +1,31 @@
 import os
 
+from _pydecimal import Decimal
+
 from bittrex import bittrex
+
 
 def percentage(a, b):
     return round(a / b * 100, 2)
 
-def getLastPrice (currency):
+
+def getLastPrice(currency):
     market = '{0}-{1}'.format(trade, currency)
     summary = api.getmarketsummary(market)
     price = summary[0]['Last']
     return price
+
+
 # Get these from https://bittrex.com/Account/ManageApiKey
 
-myCurrency ="NXT"
+myCurrency = "NXT"
 myPrice = 0.00004750
 api = bittrex('key', 'secret')
 trade = 'BTC'
 
-print("Aktualna cena: ", myCurrency,getLastPrice(myCurrency),"BTC ","To jest: ", percentage(getLastPrice(myCurrency),myPrice),"%", " ceny zakupu")
+currencyPrice = round(Decimal(getLastPrice(myCurrency)), 9)
+
+print("Aktualna cena: ", myCurrency, currencyPrice, "BTC ", "To jest: ", percentage(getLastPrice(myCurrency), myPrice),
+      "%", " ceny zakupu")
 
 os.system('pause')
-
-
